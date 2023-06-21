bb7de1a7dbee01d1943b3e3f8eb8012c57878fc5
Gediel Filho <63916720+GedielF@users.noreply.github.com>
2023-06-21 12:19:37 -0300

    Add files via upload

---
+++ server/target/classes/db/migration/V4__Insert_Data_In_Books.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO `books` (`author`, `launch_date`, `title`) VALUES
+	('Michael C. Feathers', '2017-11-29 13:50:05.878000', 'Working effectively with legacy code'),
+	('Ralph Johnson, Erich Gamma, John Vlissides e Richard Helm', '2017-11-29 15:15:13.636000',  'Design Patterns'),
+	('Robert C. Martin', '2009-01-10 00:00:00.000000', 'Clean Code'),
+	('Crockford', '2017-11-07 15:09:01.674000', 'JavaScript'),
+	('Steve McConnell', '2017-11-07 15:09:01.674000', 'Code complete'),
+	('Martin Fowler e Kent Beck', '2017-11-07 15:09:01.674000', 'Refactoring'),
+	('Eric Freeman, Elisabeth Freeman, Kathy Sierra, Bert Bates', '2017-11-07 15:09:01.674000', 'Head First Design Patterns'),
+	('Eric Evans', '2017-11-07 15:09:01.674000', 'Domain Driven Design'),
+	('Brian Goetz e Tim Peierls', '2017-11-07 15:09:01.674000', 'Java Concurrency in Practice'),
+	('Susan Cain', '2017-11-07 15:09:01.674000', 'O poder dos quietos'),
+	('Roger S. Pressman', '2017-11-07 15:09:01.674000', 'Engenharia de Software: uma abordagem profissional'),
+	('Viktor Mayer-Schonberger e Kenneth Kukier', '2017-11-07 15:09:01.674000', 'Big Data: como extrair volume, variedade, velocidade e valor da avalanche de informação cotidiana'),
+	('Richard Hunter e George Westerman', '2017-11-07 15:09:01.674000', 'O verdadeiro valor de TI'),
+	('Marc J. Schiller', '2017-11-07 15:09:01.674000', 'Os 11 segredos de líderes de TI altamente influentes'),
+	('Aguinaldo Aragon Fernandes e Vladimir Ferraz de Abreu', '2017-11-07 15:09:01.674000', 'Implantando a governança de TI');
